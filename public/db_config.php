9c075989f10aaaa5e9bfb6119e565e441d6c76ec
yaoshangyu <liuyunzuiwan@163.com>
2019-04-14 17:05:02 +0800

    ceshi

--- public/db_config.php
+++ public/db_config.php
@@ -3,7 +3,7 @@ return array(
     'DB_HOST'=>'localhost',
     'DB_NAME'=>'treasure',
     'DB_USER'=>'root',
-    'DB_PWD'=>'',
+    'DB_PWD'=>'aaa',
     'DB_PORT'=>'3306',
     'DB_PREFIX'=>'fanwe_',
 );
