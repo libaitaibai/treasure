ea23d0862d0463de66ff9b9e862969943692f327
lanchongyang <lancy@bestzst.com>
2019-04-28 14:30:14 +0800

    add

--- app/Lib/main/turntableModule.class.php
+++ app/Lib/main/turntableModule.class.php
@@ -16,13 +16,31 @@ class turntableModule extends MainBaseModule
         $web_article_id = intval($_REQUEST['id']);
         $is_agreement = strim($_REQUEST['t']);
         $shptel = app_conf("SHOP_TEL");
-        $page =intval($_REQUEST['p']);
-        $page_size =app_conf("PAGE_SIZE");
 
+        $actityid = strim($_REQUEST['actityid'])?$_REQUEST['actityid']:1;
+
+        $actitys = $GLOBALS['db']->getAll("select * from ".DB_PREFIX."turntable_actity where   status = 1");
+
+        if($actityid){
+            $actity = $GLOBALS['db']->getRow("select * from ".DB_PREFIX."turntable_actity where id = '".$actityid."' and status = 1");
+            $prize = $GLOBALS['db']->getAll("select * from ".DB_PREFIX."turntable_actity_prize where actityid = '".$actityid."' and status = 1");
+        }else{
+            //倒叙查找
+            $actity = $GLOBALS['db']->getRow("select * from ".DB_PREFIX."turntable_actity where status = 1 order by id desc limit 1");
+            $prize = $GLOBALS['db']->getAll("select * from ".DB_PREFIX."turntable_actity_prize where id = '".$actityid."' and status = 1");
+        }
+
+//        echo '<pre>';var_dump($prize);exit;
+
+        $GLOBALS['tmpl']->assign("prize",$prize);
+        $GLOBALS['tmpl']->assign("prize_json",json_encode($prize));
+        $GLOBALS['tmpl']->assign("actity",$actity);
+        $GLOBALS['tmpl']->assign("actity_json",json_encode($actity));
         $GLOBALS['tmpl']->assign("is_agreement",$is_agreement);
         $GLOBALS['tmpl']->assign("web_article_id",$web_article_id);
         $GLOBALS['tmpl']->assign("shptel",$shptel);
-
         $GLOBALS['tmpl']->display("turntable.html");
     }
 }
+//echo '<pre>';var_dump();exit;
+
