e06111b0cc3cd1420880f3f4bdcb7a8312e81cdd
yang.lan <496713192@qq.com>
2019-05-04 15:59:44 +0800

    ajax

--- wap/Lib/main/turntableModule.class.php
+++ wap/Lib/main/turntableModule.class.php
@@ -24,8 +24,75 @@ class turntableModule extends MainBaseModule
         $GLOBALS['tmpl']->display("turntable.html");
 
     }
+
+    /**
+     * 查看数量是否够用
+     * ?ctl=turntable&act=check
+     *         echo '/index.php?ctl=payment&act=check&class_name=Alipay';
+     */
+    public function check()
+    {
+        global_run();
+        $actityid = strim($_REQUEST['actityid']);
+        $user_info = $GLOBALS['user_info'];
+
+        if(empty($user_info)){
+            $this->Json([],500,'请先登录!');
+        }
+        if(empty($actityid) ){
+            $this->Json([],500,'数据传输不全');
+        }
+
+        $actity = $GLOBALS['db']->getRow("select * from ".DB_PREFIX."turntable_actity where id = '".$actityid."' and status = 1");
+        $type = $this->source[$actity['type']];
+        $field = 'id,user_name,'.$type;
+        $use =  $GLOBALS['db']->getRow("select {$field} from ".DB_PREFIX."user where id = '".$user_info['id']."'");
+
+        if($use[$type]<$actity['expenditure']){
+            $this->Json([],500,"没有足够的{$this->type[$actity['type']]}");
+        }else{
+            //减去消费的钻石
+            $GLOBALS['db']->getRow("update ".DB_PREFIX."user set `{$type}` = `{$type}`-{$actity['expenditure']} WHERE `id` = {$user_info['id']}");
+            $this->Json([],200,'成功!');
+        }
+    }
+
+    /**
+     * 保存中奖结果
+     */
+    public function save()
+    {
+        $prizeid = strim($_REQUEST['prizeid']);
+        global_run();
+        $user_info = $GLOBALS['user_info'];
+
+        if(empty($user_info)){
+            $this->Json([],500,'请先登录!');
+        }
+        if(empty($prizeid) ){
+            $this->Json([],500,'数据传输不全');
+        }
+
+        $prize =  $GLOBALS['db']->getRow("select * from ".DB_PREFIX."turntable_actity_prize where id = '".$prizeid."'");
+        if($prize['count'] < 1){
+            $this->Json([],500,'奖品库存不足!');
+        }
+
+        //用户添加奖品,非实物类型
+        if(in_array($prize['type'],array_keys($this->source))){
+            $type = $this->source[$prize['type']];
+            $money = (int)$prize['name'];
+            $GLOBALS['db']->getRow("update ".DB_PREFIX."user set `{$type}` = `{$type}`+{$money} WHERE `id` = {$user_info['id']}");
+        }
+        //奖品的商品数量-1
+        $GLOBALS['db']->getRow("update ".DB_PREFIX."turntable_actity_prize set `{count}` = `{count}`-1 WHERE `id` = {$prizeid}");
+
+        $save_data = ['actityid'=>$prize['actityid'],'prizeyid'=>$prize['id'],'userid'=>$user_info['id'],'name'=>$prize['name']];
+        $GLOBALS['db']->autoExecute(DB_PREFIX."turntable_win",$save_data,'INSERT','','SILENT');
+//        $insert_id = $GLOBALS['db']->insert_id();
+
+        $this->Json([],200,'恭喜中奖!');
+
+    }
 }
-?>
 
-
-<!--echo '<pre>';var_dump();exit;-->
